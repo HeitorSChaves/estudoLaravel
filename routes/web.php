7f818bcb434e81a774b7584653d31de1e160392c
HeitorSChaves <contatoheitorsantaniello@gmail.com>
2023-02-08 17:41:55 -0300

    Adicionando mais rotas e apelidos/ encapsulando rotas

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,33 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\sobreNosController;
 use App\Http\Controllers\contatoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', [PrincipalController::class, 'principal'])->name('/.principal');
 
-Route::get('/sobre-nos', [sobreNosController::class, 'principal'])->name('sobre-nos.principal');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/contato', [contatoController::class, 'principal'])->name('contato.principal');
+Route::get('/sobre-nos', [sobreNosController::class, 'principal'])->name('site.sobrenos');
 
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function (string $nome, string $categoria, string $assunto, string $mensagem) {
-    echo "Olá, me chamo $nome e gostaria de falar com a equipe de $categoria.<br>
-    O assunto é $assunto.<br>$mensagem";
+Route::get('/contato', [contatoController::class, 'principal'])->name('site.contato');
+
+Route::get('/login', function () {
+  return 'Login'; })->name('site.login');
+
+//Criando prefixo e agrupando rotas 
+
+Route::prefix('/app') -> group(function(){
+
+  Route::get('/clientes', function () {
+    return 'Clientes'; }) -> name('app.clientes');
+
+  Route::get('/fornecedores', function () {
+    return 'Fornecedores'; }) -> name('app.fornecedores');
+
+  Route::get('/produtos', function () {
+    return 'Produtos'; }) -> name('app.produtos');
 });
 
-//Nome, categoria, assunto, mensagem
+
+
 /*
 Verbo http
 
