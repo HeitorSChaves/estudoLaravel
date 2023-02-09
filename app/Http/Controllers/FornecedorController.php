a5d30658863e1b56a6bc357dce3140b45b884e72
Matheus Constantino <contatoheitorsantaniello@gmail.com>
2023-02-09 16:38:40 -0300

    Trabalhando com imagens e contagem de fornecedores

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,26 +10,33 @@ class FornecedorController extends Controller
     {
         $fornecedores = [
             0 => [
-                'nome' =>'Fornecedor 1',
+                'nome' =>'Heitor',
                 'status' => 'S',
                 'cnpj' => '111.111.111/11',
                 'ddd' => '12',
                 'telefone' => '997055051'
             ],
             1 => [
-                'nome' =>'Fornecedor 2',
+                'nome' =>'Pedro',
                 'status' => 'S',
                 'cnpj' => '470.570.918/77',
                 'ddd' => '12',
                 'telefone' => '41410939'
             ],
             2 => [
-                'nome' =>'Fornecedor 3',
+                'nome' =>'José',
                 'status' => 'S',
                 'cnpj' => '000.000.000-00',
                 'ddd' => '12',
                 'telefone' => '996233693'
-              ]
+            ],
+            3 => [
+                'nome' => 'Rafael',
+                'status' => 'S',
+                'cnpj' => '145.768.435.23',
+                'ddd' => '11',
+                'telefone' => '981521650'
+            ]
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -10,8 +10,10 @@
 
 {{-- @unless executa caso o retorno seja false, é o mesmo de if com ! --}}
 
-@isset($fornecedores)
+
     @forelse ($fornecedores as $indice => $fornecedor)
+        {{$loop->iteration}}.
+        <br>
         Fornecedor: {{$fornecedor['nome']}}
         <br>
         Status: {{$fornecedor['status']}}
@@ -19,9 +21,18 @@
         CNPJ: {{$fornecedor['cnpj'] ?? 'Não foi preenchido'}}
         <br>
         Telefone: ({{$fornecedor['ddd']?? ''}}) {{$fornecedor['telefone']??''}}
+        <br>
+
+        @if ($loop->first)
+            1º Fornecedor
+        @endif
+        @if ($loop->last)
+            Ultimo Fornecedor
+            <br>
+            <br>
+            Total de registros: {{$loop->count}}
+        @endif
         <hr>
-    @empty
+        @empty
         Não existem fornecedores cadastrados.
     @endforelse
-@endisset
-
